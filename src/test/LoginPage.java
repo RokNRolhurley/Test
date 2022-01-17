53fde124d3d8dadb7fe64e754b52278650efa1be
Sakil <mahbub.qa7@gmail.com>
2022-01-16 23:54:13 -0500

    test push 2

---
+++ src/test/LoginPage.java
@@ -0,0 +1,11 @@
+package test;
+
+public class LoginPage {
+
+    public static void main(String a[]){
+        System.out.println("Hello git");
+        String un ="admin";
+        String pwd ="admin";
+        System.out.println("logged in..");
+    }
+}
